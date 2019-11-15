073078571a08b46b31cc4998b98ce87a85f94f4f
Federico <fede.maidan@gmail.com>
2019-11-15 11:42:39 -0300

    remove all tests

--- packages/searching-plus-plus/app/middlewares/staticFilesMiddleware.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const express = require('express');
-const path = require('path');
-
-module.exports = express.static(path.join(__dirname, '../public/'))
--- packages/searching-plus-plus/client/components/result.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ({ result = {id: '', street: '??', number: '???'} }) => (
-    <li className="result">
-        {result.id} - {result.street} {result.number}
-    </li>
-);

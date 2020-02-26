8de70f8e50fb7424b4a40cfef124516546c32d13
Giuliano <giuliano.b.marinella@gmail.com>
2020-02-26 00:14:13 -0300

    Updating question-datail view

--- packages/2.questions-search/src/app/views/questions/question-detail/question-detail.component.ts
+++ packages/2.questions-search/src/app/views/questions/question-detail/question-detail.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Question } from 'src/app/services/question';
+import { QuestionsService } from 'src/app/services/questions.service';
+
 
 @Component({
   selector: 'app-question-detail',
   templateUrl: './question-detail.component.html',
   styleUrls: ['./question-detail.component.scss']
 })
+
 export class QuestionDetailComponent implements OnInit {
+  
+  questions: Array<Question>
+  ARGS: Question[]
+  public id: string;
+  
+  questionDetail: Question;
+  constructor(private route: ActivatedRoute, private questionsSvc: QuestionsService) {
+    this.questionsSvc.questions$.subscribe(q => {
+      this.questions = q;
+    });
 
-  constructor() { }
-
-  ngOnInit() {
   }
-
+  
+  ngOnInit() {
+    this.questionsSvc.getById(parseInt(this.route.snapshot.paramMap.get('id'))).subscribe(q => {
+      this.questionDetail = q
+      console.log(q)
+    })
+  }
+  public saludar(){
+    this.questionsSvc.remove(this.questionDetail)
+  }
 }
